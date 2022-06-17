7290ac7726dd35bb3b947e8cda8ad465fb9be940
aor2405 <a.o-r@hotmail.com>
2022-06-17 14:23:08 +0100

    deployment fixes

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,15 @@
+import React, { useEffect } from 'react';
+import ReactGA from 'react-ga';
+
 import '../styles/globals.css';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    ReactGA.initialize('G-0X45ZENJ0M');
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
   return (
     <>
       <Navbar />
