089b95a6451f4e0ea8573ad27d7a1caee98470aa
aor2405 <a.o-r@hotmail.com>
2022-06-17 15:29:08 +0100

    added google tracking

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,34 @@
 import React, { useEffect } from 'react';
 import ReactGA from 'react-ga';
+import Script from 'next/script';
 
 import '../styles/globals.css';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 
 function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    ReactGA.initialize('UA-232331283-1');
-    ReactGA.pageview(window.location.pathname + window.location.search);
-  }, []);
+  // useEffect(() => {
+  //   ReactGA.initialize('UA-232331283-1');
+  //   ReactGA.pageview(window.location.pathname + window.location.search);
+  // }, []);
   return (
     <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script strategy="lazyOnload">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+        page_path: window.location.pathname,
+        });
+    `}
+      </Script>
+
       <Navbar />
       <Component {...pageProps} />
       <Footer />
