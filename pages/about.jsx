db907d1f4f2e6823546b9109432a3cf6e979e25b
aor2405 <a.o-r@hotmail.com>
2022-06-17 14:04:20 +0100

    deployment fixes

--- pages/about.jsx
+++ pages/about.jsx
@@ -15,8 +15,8 @@ export default function about() {
               About me
             </h3>
             <p className="pt-6 text-xl font-light font-paragraph col-span-8 mx-2 sm:mt-0 sm:text-2xl">
-              I graduated college in 2014 with a degree in electronic
-              engineering. Altough I started my career in engineering, I also
+              I graduated from college in 2014 with a degree in electronic
+              engineering. Although I started my career in engineering, I also
               had an interest in web and software development. With this
               interest, I continued to study web dev in my spare time and
               continued to grow my skills over the years. I made the transition
@@ -26,9 +26,9 @@ export default function about() {
             <p className="pt-6 text-xl font-light font-paragraph col-span-8 mx-2 sm:mt-0 sm:text-2xl">
               Outside of software development, my biggest passion in life in
               Basketball. I have played semi – professionally basketball for the
-              past 5 years and been awarded the roll of team captain for 3 of
+              past 5 years and been awarded the role of team captain for 3 of
               those years. I helped lead my team to the highest league in
-              Ireland where we competed in the past season.
+              Ireland, where we competed in the past season.
             </p>
           </div>
         </div>
