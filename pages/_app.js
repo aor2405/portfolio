2fdf8cea75cd42d61b70575c03bd06a002783af2
aor2405 <a.o-r@hotmail.com>
2022-06-20 11:08:11 +0100

    added google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Script from 'next/script';
 
 import '../styles/globals.css';
 import Navbar from '../components/Navbar';
@@ -8,22 +7,6 @@ import Footer from '../components/Footer';
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Script
-        strategy="lazyOnload"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-      />
-
-      <Script strategy="lazyOnload">
-        {`
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-        page_path: window.location.pathname,
-        });
-    `}
-      </Script>
-
       <Navbar />
       <Component {...pageProps} />
       <Footer />
---
+++ pages/_document.js
@@ -0,0 +1,43 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class CustomDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html lang="en-US">
+        <Head>
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+            `,
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-BCEXXXXM5K"
+          ></script>
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default CustomDocument;
